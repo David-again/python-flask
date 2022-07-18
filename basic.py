f9bb86e4816f0ca2eb5a1b4e6179da93ad358463
David Ogunbanjo <davidogunbanjo@Davids-MacBook-Air.local>
2022-07-17 22:20:08 -0400

    Flask: Routing

--- basic.py
+++ basic.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # home route
 def index(): 
-    return "<h1>Hello Flask World!</h1>"
+    return "<h1>Hello Puppies!</h1>"
+
+@app.route('/information') # a static route
+def info(): 
+    return "<h1>Puppies are cute</h1>"
+
+@app.route('/puppy/<name>')
+def puppy(name):
+    # return f"<h1>This is a page for {name}<h1>"
+    return "100th letter: {}".format(name[100])
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
